c638f46eae2aa3a1d27a9bf399c9ff5f017f961c
GrupoSointec <leosanchez_19@hotmail.com>
2022-05-16 21:59:17 -0500

    configurar ssl

--- conexion.php
+++ conexion.php
@@ -5,18 +5,7 @@ $db_user = "eduar";
 $db_pass = "Edwar20*";
 $db_name = "optycolor";
 
-//$con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-
-$mysqli = mysqli_init();
-if (!$mysqli) {
-  die("mysqli_init failed");
-}
-
-$mysqli -> ssl_set("key.pem", "cert.pem", "cacert.pem", NULL, NULL);
-
-if (!$mysqli -> real_connect($db_host,$db_user,$db_pass,$db_name)) {
-  die("Connect Error: " . mysqli_connect_error());
-}
+$con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
 if(mysqli_connect_errno()){
 	echo 'No se pudo conectar a la base de datos : '.mysqli_connect_error();
