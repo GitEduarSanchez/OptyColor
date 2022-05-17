c2b96179c47098a9cda01db58459324faf5fd2c3
GrupoSointec <leosanchez_19@hotmail.com>
2022-05-16 23:14:43 -0500

    añadiendo sll

--- conexion.php
+++ conexion.php
@@ -10,4 +10,22 @@ $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 if(mysqli_connect_errno()){
 	echo 'No se pudo conectar a la base de datos : '.mysqli_connect_error();
 }
+/* $host = 'mydemoserver.mysql.database.azure.com';
+$username = 'myadmin@mydemoserver';
+$password = 'your_password';
+$db_name = 'your_database';
+
+//Initializes MySQLi
+$conn = mysqli_init();
+
+mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+// Establish the connection
+mysqli_real_connect($conn, 'mydemoserver.mysql.database.azure.com', 'myadmin@mydemoserver', 'yourpassword', 'quickstartdb', 3306, NULL, MYSQLI_CLIENT_SSL);
+
+//If connection failed, show the error
+if (mysqli_connect_errno())
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}*/
 ?>
